67d73e9caec4e1516ec8ed4c760a4368f8fcf479
Mézes Ádám <mezesadam1997@gmail.com>
2022-02-20 11:35:10 +0100

    added gitignore /src/assets added jpg, png and mp4 global types

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+// Image assets
+declare module '*.jpg' {
+  export default '' as string;
+}
+declare module '*.png' {
+  export default '' as string;
+}
+
+// Video assets
+declare module '*.mp4' {
+  export default '' as string;
+}
